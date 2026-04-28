3e65bc12e769eb1e5901b9bf3ed09cc7a2523302
Suhanichauhan2345 <Suhanichouhan967@gamil.com>
2026-04-28 15:41:53 +0530

    update

---
+++ Index.java/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+public static void main(String[] args) {
+  int[] arr = {1,2,3,4,5,6,7};
+     int k =2;
+    int n = arr.length;
+    int temp[] = new int[n];
+    for(int i =0; i<arr.length; i++){
+        for(int j = k; j<=i; j++) {
+          //System.out.print(arr[i]);
+            temp[i] = arr[j];  
+        }
+        System.out.print(temp[i]+ " ");
+      }
+   }
+}
+
+
+
