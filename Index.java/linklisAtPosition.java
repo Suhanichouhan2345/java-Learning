5948b49f207722014c4d7a147678f7844c5a519f
Suhanichauhan2345 <Suhanichouhan967@gamil.com>
2026-06-25 16:01:50 +0530

    code

--- Index.java/linklisAtPosition.java
+++ Index.java/linklisAtPosition.java
@@ -7,10 +7,13 @@ class Node {
         this.next = null;
     }
 }
+
 class LinkedList {
     Node head;
+
     public void insertAtPosition(int data, int position) {
         Node newNode = new Node(data);
+
         if (position == 0) {
             newNode.next = head;
             head = newNode;
@@ -18,6 +21,7 @@ class LinkedList {
         }
 
         Node temp = head;
+
         for (int i = 0; i < position - 1; i++) {
             if (temp == null) {
                 System.out.println("Invalid position");
@@ -34,11 +38,12 @@ class LinkedList {
         newNode.next = temp.next;
         temp.next = newNode;
     }
+
     public void display() {
         Node temp = head;
 
         while (temp != null) {
-            System.out.print(temp.data + " ");
+            System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
 
@@ -49,12 +54,16 @@ class LinkedList {
 public class linklisAtPosition {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+
         list.insertAtPosition(10, 0);
         list.insertAtPosition(20, 1);
         list.insertAtPosition(30, 2);
+
         System.out.println("Original List:");
         list.display();
+
         list.insertAtPosition(25, 2);
+
         System.out.println("After Inserting 25 at Position 2:");
         list.display();
     }
